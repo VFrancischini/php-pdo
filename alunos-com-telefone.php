484c37288c25391242bef04d09a6c11507f4556b
VFrancischini <vfrancischini@outlook.com>
2022-05-19 12:19:02 -0300

    Resolvendo o problema de N + 1 de queries

---
+++ alunos-com-telefone.php
@@ -0,0 +1,19 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+$repository = new PdoStudentRepository($connection);
+
+/**
+ * @var Student[] $studentList
+ */
+$studentList = $repository->studentsWithPhones();
+
+echo $studentList[2]->phones()[0]->formattedPhone() . PHP_EOL;
+
+print_r($studentList);
